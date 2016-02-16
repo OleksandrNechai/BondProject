a24cdf9dd07f80e361b10705f176ef79e94bb065
OleksandrNechai <nechais@gmail.com>
2016-02-16 21:14:33 +0200

    Apply module pattern to the core calculation functions

--- src/core.js
+++ src/core.js
@@ -1,8 +1,16 @@
-cleanPrice = function(bond) {
-    var couponPaymentsCount = bond.term * bond.couponFrequency;
-    var couponValue = bond.nominal * bond.couponRate / bond.couponFrequency;
-    var requiredMarketYield = bond.marketRate / bond.couponFrequency;
-    var n = Math.pow(1 + requiredMarketYield, couponPaymentsCount);
-    var price = couponValue * ((1 - (1 / n))) / requiredMarketYield + bond.nominal / n;
-    return price;
-}
+var core = (function() {
+
+    return {
+        cleanPrice: cleanPrice
+    };
+
+    function cleanPrice(bond) {
+        var couponPaymentsCount = bond.term * bond.couponFrequency;
+        var couponValue = bond.nominal * bond.couponRate / bond.couponFrequency;
+        var requiredMarketYield = bond.marketRate / bond.couponFrequency;
+        var n = Math.pow(1 + requiredMarketYield, couponPaymentsCount);
+        var price = couponValue * ((1 - (1 / n))) / requiredMarketYield + bond.nominal / n;
+        return price;
+    }
+    
+})();
--- src/tests/core_tests.js
+++ src/tests/core_tests.js
@@ -1,6 +1,6 @@
 describe("Valid data is passed", function() {
     it("should calculate clean bond price", function() {
-        var p = cleanPrice({
+        var p = core.cleanPrice({
             nominal: 1000,
             term: 10,
             couponRate: 0.1,
