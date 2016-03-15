d2ea077689400ddc9100a68e2d18573025bfd205
ivan-mudrak <ivan.mudrak@ukr.net>
2016-03-15 12:57:18 +0200

    cash flow almost done :)

--- src/app.js
+++ src/app.js
@@ -1,17 +1,39 @@
 var app = (function () {
     return {
-        calcCleanPrice: calcCleanPrice
+        calcPrice: calcDirtyPrice,
+        calcCashFlow: calcCashFlow
     };
 
-    function calcCleanPrice() {
-        var price = core.cleanPrice({
+    function calcDirtyPrice() {
+        var price = core.dirtyPrice({
             nominal: $('#nominal').val(),
             term: $('#term').val(),
+            bondtype: $('#bondtype').val(),
             couponRate: $('#couponrate').val() / 100,
+            couponFrequency: $('#couponfrequency').val(),
+            daycountconvention: $('#daycountconvention').val(),
+            maturitydate: $('#maturitydate').val(),
             marketRate: $('#marketrate').val() / 100,
-            couponFrequency: $('#couponfrequency').val()
+            date: $('#date').val()
         });
         $('#bondprice').val(round(price));
+    }
+
+    function calcCashFlow() {
+        var cashflow = core.cashFlow({
+            nominal: $('#nominal').val(),
+            term: $('#term').val(),
+            bondtype: $('#bondtype').val(),
+            couponRate: $('#couponrate').val() / 100,
+            couponFrequency: $('#couponfrequency').val(),
+            daycountconvention: $('#daycountconvention').val(),
+            maturitydate: $('#maturitydate').val(),
+            marketRate: $('#marketrate').val() / 100,
+            date: $('#date').val()
+        });
+        $('#cashflow').bootstrapTable('load', //cashflow
+            {date: '1212', coupon: 12, redemption: 10, total: 22}
+        );
     }
 
     function round(num) {
--- src/core.js
+++ src/core.js
@@ -1,16 +1,124 @@
 var core = (function() {
 
     return {
-        cleanPrice: cleanPrice
+        dirtyPrice: dirtyPrice,
+        cashFlow: cashFlow
     };
+
+    function dirtyPrice(bond) {   
+        return cleanPrice(bond) + accruedInterest(bond);
+    }
 
     function cleanPrice(bond) {
         var couponPaymentsCount = bond.term * bond.couponFrequency;
         var couponValue = bond.nominal * bond.couponRate / bond.couponFrequency;
         var requiredMarketYield = bond.marketRate / bond.couponFrequency;
         var n = Math.pow(1 + requiredMarketYield, couponPaymentsCount);
-        var price = couponValue * ((1 - (1 / n))) / requiredMarketYield + bond.nominal / n;
-        return price;
+        var cleanprice = couponValue * ((1 - (1 / n))) / requiredMarketYield + bond.nominal / n;     
+        return cleanprice;
+    }
+
+    function accruedInterest(bond) {
+        var accruedinterest = getNextPaymentAmount(bond);
+        accruedinterest = Math.pow(accruedinterest, 1);
+        return accruedinterest;
+    }
+
+    function daysBetweenYears(dayCountConvention, fromYear, toYear) {
+        switch(dayCountConvention)
+        {
+
+        }
+        return days;
+    }
+
+    function daysToEndOfYear(dayCountConvention, date) {
+
+    }
+
+    function daysFromBeginOfYear(dayCountConvention, date) {
+
+    }
+
+    function getNextPaymentAmount(bond)
+    {
+        return 1;
+    }
+
+    function cashFlow(bond) {
+        var cashFlow = [];       
+
+        var numberOfPayments = bond.term;
+        switch(bond.couponFrequency)
+        {
+            case 'Monthly':
+                numberOfPayments *= 12;
+                break;
+            case 'Quarterly':
+                numberOfPayments *= 4;
+                break;
+            case 'Semi-Annual':
+                numberOfPayments *= 2;
+                break;
+            default:
+        }
+
+        switch (bond.bondType)
+        {
+            case 'Bullet':
+                for (var i = 0; i < numberOfPayments; i++) {
+                    var couponAmount = ond.nominal * bond.couponRate;
+                    if (i == numberOfPayments - 1)  {
+                        cashFlow.push({
+                            date: '2016-01-01',
+                            coupon: couponAmount,
+                            redemption: 0,
+                            total: couponAmount,
+                        });
+                    } else {
+                        cashFlow.push({
+                            date: '2016-01-01',
+                            coupon: couponAmount,
+                            redemption: bond.nominal,
+                            total: bond.nominal + couponAmount,
+                        });
+                    }                   
+                }
+                break;
+
+            case 'Serial':
+                var remainingNominal = bond.nominal;
+                var redemptionAmount = bond.nominal / numberOfPayments;
+                for (var i = 0; i < numberOfPayments; i++) {
+                    cashFlow.push({
+                        date: '2016-01-01',
+                        coupon: remainingNominal * bond.couponRate,
+                        redemption: redemptionAmount,
+                        total: redemptionAmount + (remainingNominal * bond.couponRate),
+                    });
+                    remaingNominal -= redemptionAmount;
+                }
+                break;
+
+            case 'Annual':
+                var remainingNominal = bond.nominal;
+                var redemptionAmount = (bond.nominal * ((1 + bond.couponRate) - 1)) / (Math.pow(1 + bond.couponRate, numberOfPayments) - 1);
+                for (var i = 0; i < numberOfPayments; i++) {
+                    cashFlow.push({
+                        date: '2016-01-01',
+                        coupon: remainingNominal * bond.couponRate,
+                        redemption: redemptionAmount,
+                        total: redemptionAmount + (remainingNominal * bond.couponRate),
+                    });
+                    remaingNominal -= redemptionAmount;
+                    redemptionAmount *= 1 + bond.couponRate;
+                }
+                break;
+
+            default:
+        }
+
+        return cashflow;
     }
     
 })();
